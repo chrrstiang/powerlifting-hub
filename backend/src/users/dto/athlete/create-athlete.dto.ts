2caf6cf0d7826f18195c9cea19ef1029e96c8b7b
chrrstiang <garcia.chris@northeastern.edu>
2025-07-15 20:31:43 -0400

    changes

--- backend/src/users/dto/athlete/create-athlete.dto.ts
+++ backend/src/users/dto/athlete/create-athlete.dto.ts
@@ -1,7 +1,5 @@
-import { Coach } from "src/users/entities/user.coach";
 import { CreateUserDto } from "../create-user.dto";
 import { Program } from "src/program/entities/program.entity";
-import { Type } from 'class-transformer'
 import { IsEnum, IsString } from 'class-validator' 
 import { UserRole } from "src/users/entities/user.abstract";
 
